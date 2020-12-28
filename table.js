1d10cd88dcec2e49e5481943ac4ee3f800d70ae8
savisch <savisch@gmail.com>
2020-12-28 09:39:57 -0600

    Delete table.js (move to script.js)

--- table.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("first-name").textContent=localStorage.firstName;
-document.getElementById("last-name").textContent=localStorage.lastName;
-document.getElementById("textarea-text").textContent=localStorage.text;
